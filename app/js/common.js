930c965e3d284500ee1577be2ae0f861c4e75d4a
Mark Sheludko <mark.sheludko@gmail.com>
2018-02-11 21:02:36 +0200

    Header main settings done
    Need to upgrade:
    -Gamburger menu
    -Popup video by button
    -Hover effect for nav

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+// Popup video
+$('.video-link').magnificPopup({
+    type: 'inline',
+    callbacks: {
+      open: function() {
+        this.content.children('video')[0].play();
+      },
+      close: function() {
+        this.content.children('video')[0].pause();
+      }
+    }
+});
